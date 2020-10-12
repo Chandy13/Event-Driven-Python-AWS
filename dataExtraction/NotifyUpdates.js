5526f3365f55f2cfc553954e50b7f2a79fc599c3
Chandy13 <60304364+Chandy13@users.noreply.github.com>
2020-10-12 00:49:51 -0400

    added dynamodb cfn + update js script

---
+++ dataExtraction/NotifyUpdates.js
@@ -0,0 +1,30 @@
+'use strict';
+var AWS = require("aws-sdk");
+var sns = new AWS.SNS();
+
+exports.handler = (event, context, callback) => {
+
+    event.Records.forEach((record) => {
+        console.log('Stream record: ', JSON.stringify(record, null, 2));
+
+        if (record.eventName == 'INSERT') {
+            var date = JSON.stringify(record.dynamodb.NewImage.Date.S);
+            var cases = JSON.stringify(record.dynamodb.NewImage.Cases.N);
+            var deaths = JSON.stringify(record.dynamodb.NewImage.Deaths.N);
+            var recoveries = JSON.stringify(record.dynamodb.NewImage.Recoveries.N);
+            var params = {
+                Subject: 'US Covid Data update',
+                Message: `Successfully processed ${event.Records.length} records. On ${date}, there were ${cases} cases, ${deaths} deaths, and ${recoveries} recoveries`,
+                TopicArn: 'arn:aws:sns:us-east-1:018943110893:NotifyMe'
+            };
+            sns.publish(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to send message. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("Results from sending message: ", JSON.stringify(data, null, 2));
+                }
+            });
+        }
+    });
+    callback(null, `Successfully processed ${event.Records.length} records.`);
+};   
--- dataExtraction/dataLoader.py
+++ dataExtraction/dataLoader.py
@@ -1,4 +1,35 @@
 from dataManipulation import dataManipulation
 import boto3
+import botocore
 
-print(dataManipulation()[0])
+client = boto3.client('dynamodb')
+
+def dataLoader():
+    for dataSet in dataManipulation():
+        try:
+            client.put_item( #add response = at beginning if function doesn't work
+                TableName = 'USCOVIDDATA',
+                Item = {
+                    'Date' : {
+                        'S' : dataSet[0]
+                    },
+                    'Cases' : {
+                        'N' : dataSet[1]
+                    },
+                    'Deaths' : {
+                        'N' : dataSet[2]
+                    },
+                    'Recoveries' : {
+                        'N' : dataSet[3]
+                    }
+                },
+                ExpressionAttributeNames={'#date': 'Date'},
+                ConditionExpression='attribute_not_exists(#date)'
+            )
+        except botocore.exceptions.ClientError as e:
+            # Ignore the ConditionalCheckFailedException
+            # other exceptions.
+            if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+                raise
+
+dataLoader()
